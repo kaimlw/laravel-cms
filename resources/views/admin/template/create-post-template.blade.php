c5cf809a66295a7d1daffbeb2a9c6348095d0c19
kaimlw <muhammad.yulian10@gmail.com>
2025-01-26 21:37:30 +0800

    fix: favicon create post template

--- resources/views/admin/template/create-post-template.blade.php
+++ resources/views/admin/template/create-post-template.blade.php
@@ -18,15 +18,14 @@
     <link rel="stylesheet" href="{{ asset('assets/admin/assets/css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/admin/assets/css/create-post.css') }}">
 
+    
+
     {{-- FAVICON --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('assets/site.webmanifest') }}">
-
-    {{-- CKEditor 5 --}}
-    {{-- <script src="{{ asset('assets/vendor/ckeditor5/ckeditor.js') }}"></script>     --}}
-
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    
     {{-- CSS AddOn --}}
     @yield('css-addOn')
     <style>
