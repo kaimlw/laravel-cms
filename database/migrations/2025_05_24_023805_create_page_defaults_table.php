5136c027f9cc0a569a2bd4eb737145f2049187a2
kaimlw <muhammad.yulian10@gmail.com>
2025-05-24 10:48:11 +0800

    fix: remove softDeletes in post migration

---
+++ database/migrations/2025_05_24_023805_create_page_defaults_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('page_defaults', function (Blueprint $table) {
+            $table->id();
+            $table->integer('author');
+            $table->string('title')->nullable();
+            $table->string('slug')->unique()->nullable();
+            $table->longText('content')->nullable();
+            $table->string('excerpt')->nullable();
+            $table->enum('type', ['post','page']);
+            $table->enum('status', ['draft', 'publish']);
+            $table->string('banner_post_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('page_defaults');
+    }
+};
